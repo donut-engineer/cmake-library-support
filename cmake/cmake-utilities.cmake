02087ee8221e3a4ec3c2ecacb92ef698404cd589
Trevor Carter <donut-engineer@users.noreply.github.com>
2026-04-07 17:13:45 -0600

    fix bad return in cmake utils include

--- cmake/cmake-utilities.cmake
+++ cmake/cmake-utilities.cmake
@@ -10,12 +10,9 @@
 #   FetchContent_MakeAvailable(cmake_utilities)
 #   include(${cmake_utilities_SOURCE_DIR}/cmake/cmake-utilities.cmake)
 
-# Guard against multiple inclusions
-if(DEFINED CMAKE_UTILITIES_DIR)
-    return()
-endif()
-
 set(CMAKE_UTILITIES_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE INTERNAL
     "Path to cmake-utilities cmake/ directory")
 
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_UTILITIES_DIR}")
+if(NOT "${CMAKE_UTILITIES_DIR}" IN_LIST CMAKE_MODULE_PATH)
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_UTILITIES_DIR}")
+endif()
